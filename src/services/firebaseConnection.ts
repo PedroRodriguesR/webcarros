0e77ca7f7ea1e4c08975551c386c43c99611cf18
Pedro Rojas <pedro.rodrigues.rojas@gmail.com>
2025-01-13 19:33:26 -0300

    Removendo dados do firebase

--- src/services/firebaseConnection.ts
+++ src/services/firebaseConnection.ts
@@ -5,13 +5,10 @@ import { getFirestore } from 'firebase/firestore'
 import { getStorage } from 'firebase/storage'
 
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAkwJFFHDIUFiLj60c9R1x7PogZmuqwthY",
-  authDomain: "webcarros-c5b8b.firebaseapp.com",
-  projectId: "webcarros-c5b8b",
-  storageBucket: "webcarros-c5b8b.appspot.com",
-  messagingSenderId: "880717485089",
-  appId: "1:880717485089:web:e878f6b9792afdc9e7bf60"
+  //informações confidenciais
+
 };
 
 const app = initializeApp(firebaseConfig);
